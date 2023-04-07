8ae3b9f1ab5dbcd14526b0ea685c70a4f1bb4b90
Masoud <masoud.zeinalabedini@gmail.com>
2023-04-07 14:36:48 +0330

    Fix spelling error in classname of  bookmarkBtn

--- contentScript.js
+++ contentScript.js
@@ -17,17 +17,17 @@
       const bookmarkBtn = document.createElement("img");
 
       bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-      bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+      bookmarkBtn.className = "ytp-button bookmark-btn";
       bookmarkBtn.title = "Click to bookmark current timestamp";
 
       youtubeLeftControls =
-        document.getElementsByClassName("ytp-left-controls")[0];
+        document.getElementsByClassName("ytp-right-controls")[0];
       youtubePlayer = document.getElementsByClassName("video-stream");
 
-      youtubeLeftControls.appendChild(bookmarkBtn);
-      //   bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+      youtubeLeftControls.prepend(bookmarkBtn);
+      bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
     }
   };
 })();
 
-//26:00
+
